3c7fb717dca1d13b2083e814a67153ca09d7e3f2
wipddsu <rlaalstn8158@naver.com>
2024-03-15 11:31:25 +0900

    스타일 마무리

--- src/components/Content.js
+++ src/components/Content.js
@@ -13,32 +13,50 @@ export default function Content({ projects, projectIndex, onSubmit, onProjectDel
     <section className="flex-auto w-3/4 h-full py-20 px-10">
       {projects.length > 0 ? (
         <>
-          <div id="projectInfo">
+          <div id="projectInfo" className="pb-7 border-b border-b-slate-200">
             <div className="flex felx-row justify-between">
               <div>
-                <h2 className="text-3xl">{project.title}</h2>
-                <span>{project.date}</span>
+                <h2 className="text-3xl mb-3 font-bold">{project.title}</h2>
+                <span className="text-stone-500">{project.date}</span>
               </div>
-              <button onClick={() => onProjectDelete(projectIndex)}>Delete</button>
+              <div className="flex flex-row items-start">
+                <button onClick={() => onProjectDelete(projectIndex)} className="text-stone-500 hover:text-stone-800">
+                  Delete
+                </button>
+              </div>
             </div>
-            <p>{project.description}</p>
+            <p className="mt-7 leading-normal" style={{ whiteSpace: 'pre-line' }}>
+              {project.description}
+            </p>
           </div>
-          <div id="tasks">
+          <div id="tasks" className="mt-7">
             <h3 className="text-2xl">Tasks</h3>
             <form
               onSubmit={(e) => {
                 onSubmit(e, projectIndex, taskInput);
                 setTaskInput('');
               }}
+              className="flex flex-row my-7"
             >
-              <input type="text" id="task" name="task" value={taskInput} onChange={handleTaskInput} required />
-              <button>Add Task</button>
+              <input
+                type="text"
+                id="task"
+                name="task"
+                value={taskInput}
+                onChange={handleTaskInput}
+                required
+                className="block rounded-md border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mr-4 w-60"
+                placeholder="Add your Task"
+              />
+              <button className="text-indigo-600 hover:text-indigo-950">Add Task</button>
             </form>
-            <ul>
+            <ul className="overflow-auto h-80">
               {project.tasks.map((task, index) => (
-                <li key={index} className="flex flex-row justify-between">
+                <li key={index} className="flex flex-row justify-between p-6 bg-slate-100 mb-4 rounded">
                   {task}
-                  <button onClick={() => onTaskDelete(index)}>Delete</button>
+                  <button onClick={() => onTaskDelete(index)} className="text-stone-500 hover:text-stone-800">
+                    Delete
+                  </button>
                 </li>
               ))}
             </ul>
